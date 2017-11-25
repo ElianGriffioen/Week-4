2fad69dd93dd9df86f6793949f541f63aa1703b1
ElianGriffioen <e.griffioen@students.uu.nl>
2017-11-25 16:53:07 +0100

    Initial commit

---
+++ feedback Gerko.R
@@ -0,0 +1,22 @@
+# uitleg Gerko
+
+# functies aanroepen
+require(mice)
+
+sym
+
+mice:::sym
+
+
+# floating errors
+3-2.9
+
+(3-2.9)-0.1
+
+(3-2.9)==0.1
+
+all.equal(3-2.9,0.1)
+
+## markdown
+
+# you can use warning=FALSE en message=FALSE om deze niet weer te geven
